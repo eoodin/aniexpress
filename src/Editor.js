e3a7e02ee7b76868dce346d7ca2e94c43809acb7
Liu Jeffrey <eoodin@outlook.com>
2019-01-25 09:21:12 +0800

    two box example

--- src/Editor.js
+++ src/Editor.js
@@ -21,7 +21,7 @@ export default class Editor extends Component {
             {
                 name: "Primary Cloud",
                 space: {from: {x: 0, y: 0, z: 0}, to: {x: 200, y: 240, z: 200}},
-                surface: {color: '#11AA33', opacity: 0.3},
+                surface: {color: '#11AA33', opacity: 0.1},
                 children: [
                     {
                         name: "VI",
@@ -37,17 +37,19 @@ export default class Editor extends Component {
             },
             {
                 name: "Backup Cloud",
-                space: {from: {x: 200, y: 0, z: 0}, to: {x: 200, y: 240, z: 200}},
+                space: {from: {x: 200, y: 0, z: 0}, to: {x: 400, y: 240, z: 200}},
+                surface: {color: '#1133AA', opacity: 0.2},
                 children: []
             }
         ];
 
         const links = [];
 
-        const data = [12, 4, 6, 9, 7, 10];
         this.svg = d3.select(".Editor").append("svg")
                      .attr("width", 1400).attr("height", 600);
-        this.drawBox(components[0].space, components[0].surface)
+
+        this.drawBox(components[0].space, components[0].surface);
+        this.drawBox(components[1].space, components[1].surface);
     }
 
     render() {
@@ -56,7 +58,7 @@ export default class Editor extends Component {
 
     drawBox(space, surface) {
         const origin = {x: 400, y: 300};
-        const v = {x: -0.5, y: -0.5};
+        const v = {x: -0.5, y: -0.2};
         let {from, to} = space;
         let parallelograms = [
             [ // back
@@ -66,10 +68,10 @@ export default class Editor extends Component {
                 {x: from.x + from.z * v.x, y: to.y + from.z * v.y}
             ],
             [ // bottom
-                {x: from.x + from.z * v.x, y: from.z * v.y},
-                {x: to.x + from.z * v.x, y: from.z * v.y},
-                {x: to.x + to.z * v.x, y: to.z * v.y},
-                {x: from.x + to.z * v.x, y: to.z * v.y}
+                {x: from.x + from.z * v.x, y: from.y + from.z * v.y},
+                {x: to.x + from.z * v.x, y: from.y + from.z * v.y},
+                {x: to.x + to.z * v.x, y: from.y + to.z * v.y},
+                {x: from.x + to.z * v.x, y: from.y + to.z * v.y}
             ],
             [ // front
                 {x: from.x + to.z * v.x, y: from.y + to.z * v.y},
@@ -78,8 +80,20 @@ export default class Editor extends Component {
                 {x: from.x + to.z * v.x, y: to.y + to.z * v.y}
             ],
             [// left
+                {x: from.x + from.z * v.x, y: from.y + from.z * v.y},
                 {x: from.x + to.z * v.x, y: from.y + to.z * v.y},
+                {x: from.x + to.z * v.x, y: to.y + to.z * v.y},
+                {x: from.x + from.z * v.x, y: to.y + from.z * v.y}
+            ],
+            [// right
+                {x: to.x + from.z * v.x, y: from.y + from.z * v.y},
                 {x: to.x + to.z * v.x, y: from.y + to.z * v.y},
+                {x: to.x + to.z * v.x, y: to.y + to.z * v.y},
+                {x: to.x + from.z * v.x, y: to.y + from.z * v.y}
+            ],
+            [ // top
+                {x: from.x + from.z * v.x, y: to.y + from.z * v.y},
+                {x: to.x + from.z * v.x, y: to.y + from.z * v.y},
                 {x: to.x + to.z * v.x, y: to.y + to.z * v.y},
                 {x: from.x + to.z * v.x, y: to.y + to.z * v.y}
             ],
@@ -87,13 +101,13 @@ export default class Editor extends Component {
 
         // reverse for to bottom up
         parallelograms = parallelograms.map(pl => pl.map(p => ({x: p.x, y: (p.y * -1)})));
-        console.log(parallelograms);
-        this.svg.selectAll("polygon")
+        this.svg.selectAll("polygon.p" + Math.floor(Math.random() * 10000))
             .data(parallelograms)
             .enter()
             .append("polygon")
             .attr("points", d => d.map(p => (p.x + origin.x) + ',' + (p.y + origin.y)).join(' '))
-            .attr("fill", '#33AA88')
-            .attr("fill-opacity", 0.3);
+            .attr("fill", surface.color)
+            .attr("stroke", 'darkgray')
+            .attr("fill-opacity", surface.opacity);
     }
 }
