2092adc4fb42ca00c607ca4361249fa734de4d24
Liu Jeffrey <eoodin@outlook.com>
2019-01-29 13:52:11 +0800

    make z projection adjustable

--- src/Editor.js
+++ src/Editor.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+
 import './Editor.css';
 import * as d3 from "d3";
 
@@ -6,18 +7,13 @@ export default class Editor extends Component {
     constructor(props) {
         super(props);
         this.svg = null;
-        this.boxes = []
+        this.boxes = [];
+        this.state = {zFactor: {x: -0.5, y: -0.2}};
     }
 
     componentDidMount() {
-        this.drawChart();
-    }
+        this.svg = d3.select(".Editor").append("svg").attr("width", 800).attr("height", 600);
 
-    componentWillUnmount() {
-        d3.select(".Editor").remove(this.svg);
-    }
-
-    drawChart() {
         const components = [
             {
                 name: "Primary Cloud",
@@ -44,25 +40,82 @@ export default class Editor extends Component {
                 children: []
             }
         ];
+        this.drawBox(components[0].space.from, components[0].space.to, components[0].surface);
+        this.drawBox(components[1].space.from, components[1].space.to, components[1].surface);
+        this.drawBox(components[0].children[0].space.from, components[0].children[0].space.to, components[0].children[0].surface);
 
-        const links = [];
-
-        this.svg = d3.select(".Editor").append("svg").attr("width", 1400).attr("height", 600);
-
-        this.drawBox(components[0].children[0].space, components[0].children[0].surface);
-        this.drawBox(components[0].space, components[0].surface);
-        this.drawBox(components[1].space, components[1].surface);
         this.updateChart();
     }
 
-    render() {
-        return <div className="Editor"/>
+    componentWillUnmount() {
+        d3.select(".Editor").remove(this.svg);
     }
 
-    drawBox(space, surface) {
-        const v = {x: -0.5, y: -0.2};
-        let {from, to} = space;
-        let parallelograms = [
+    changeZx(x) {
+        this.setState({zFactor: {... this.state.zFactor, x: x}});
+    }
+
+    changeZy(y) {
+        this.setState({zFactor: {... this.state.zFactor, y: y}});
+    }
+
+    render() {
+        this.updateChart();
+        return <div className="Editor">
+            <div className="controls">
+                <div>
+                    <label> Zx-rotate: {this.state.zFactor.x}
+                        <input type="range" max="1" min="-1" step="0.01" value={this.state.zFactor.x}
+                               onChange={e => this.changeZx(e.target.value)}/>
+                    </label>
+                </div>
+                <div>
+                    <label> Zy-rotate: {this.state.zFactor.y}
+                        <input type="range" max="1" min="-1" step="0.01" value={this.state.zFactor.y}
+                               onChange={e => this.changeZy(e.target.value)}/>
+                    </label>
+                </div>
+            </div>
+        </div>
+    }
+
+    drawBox(from, to, surface) {
+        let id;
+        do {
+            id = Math.floor(Math.random() * 1000000);
+        } while (this.boxes.some(b => b.id === id));
+
+        this.boxes.push({id: id, from: from, to: to, surface: surface})
+    }
+
+    updateChart() {
+        if (!this.svg) {
+            return;
+        }
+
+        const origin = {x: 300, y: 400};
+        let all = this.svg && this.svg.selectAll('polygon');
+        all && all.remove();
+        // reverse for to bottom up
+        for (let id in this.boxes) {
+            let b = this.boxes[id];
+            let faces = this.getBoxFaces(b);
+            let parallelograms = faces.map(pl => pl.map(p => ({x: p.x, y: (p.y * -1)})));
+            this.svg.selectAll("polygon.p" + b.id)
+                .data(parallelograms)
+                .enter()
+                .append("polygon")
+                .attr("points", d => d.map(p => (p.x + origin.x) + ',' + (p.y + origin.y)).join(' '))
+                .attr("fill", b.surface.color)
+                .attr("stroke", 'darkgray')
+                .attr("fill-opacity", b.surface.opacity);
+        }
+    }
+
+    getBoxFaces(b) {
+        const v = this.state.zFactor;
+        let {from, to} = b;
+        return [
             [ // back
                 {x: from.x + from.z * v.x, y: from.y + from.z * v.y},
                 {x: to.x + from.z * v.x, y: from.y + from.z * v.y},
@@ -100,29 +153,5 @@ export default class Editor extends Component {
                 {x: from.x + to.z * v.x, y: to.y + to.z * v.y}
             ]
         ];
-
-        let id;
-        do {
-            id = Math.floor(Math.random() * 1000000);
-        } while(this.boxes.some(b => b.id === id));
-
-        this.boxes.push({id: id, surface: surface, faces: parallelograms});
-    }
-
-    updateChart() {
-        const origin = {x: 400, y: 400};
-        // reverse for to bottom up
-        for (let id in this.boxes) {
-            let b = this.boxes[id];
-            let parallelograms = b.faces.map(pl => pl.map(p => ({x: p.x, y: (p.y * -1)})));
-            this.svg.selectAll("polygon.p" + b.id)
-                .data(parallelograms)
-                .enter()
-                .append("polygon")
-                .attr("points", d => d.map(p => (p.x + origin.x) + ',' + (p.y + origin.y)).join(' '))
-                .attr("fill", b.surface.color)
-                .attr("stroke", 'darkgray')
-                .attr("fill-opacity", b.surface.opacity);
-        }
     }
 }
