b3755e5834af7c0dfd3fb64e2552a4aed8fb9820
Liu Jeffrey <eoodin@outlook.com>
2019-07-15 22:56:53 +0800

    add line example

--- src/Editor.js
+++ src/Editor.js
@@ -11,23 +11,37 @@ export default class Editor extends Component {
         this.setState({zFactor: {x: -0.5, y: -0.2}});
         this.scene = new THREE.Scene();
         this.camera = new THREE.PerspectiveCamera(
-            75,
+            45,
             width / height,
             0.1,
-            1000
+            500
         );
 
-        this.camera.position.z = 4;
+        this.camera.position.set(0, 0, 100);
+        this.camera.lookAt(0, 0 ,0);
         //ADD RENDERER
-        this.renderer = new THREE.WebGLRenderer({antialias: true});
+        var canvas = document.createElement('canvas');
+        var context = canvas.getContext('webgl2');
+        this.renderer = new THREE.WebGLRenderer({canvas: canvas, context: context});
+        // this.renderer = new THREE.WebGLRenderer({antialias: true});
         this.renderer.setClearColor('#000000');
         this.renderer.setSize(width, height);
         this.mount.appendChild(this.renderer.domElement);
         //ADD CUBE
-        const geometry = new THREE.BoxGeometry(1, 1, 1);
+        const geometry = new THREE.BoxGeometry(10, 10, 10);
         const material = new THREE.MeshBasicMaterial({color: '#433F81'});
         this.cube = new THREE.Mesh(geometry, material);
         this.scene.add(this.cube);
+
+        const geo = new THREE.Geometry();
+        geo.vertices.push(new THREE.Vector3(-10, 0, 0));
+        geo.vertices.push(new THREE.Vector3(0, 10, 0));
+        geo.vertices.push(new THREE.Vector3(10, 0, 0));
+
+        const mat = new THREE.LineBasicMaterial({color: 0x0000ff});
+        const line = new THREE.Line(geo, mat);
+        this.scene.add(line);
+
         this.start();
     }
 
@@ -60,9 +74,9 @@ export default class Editor extends Component {
     render() {
         return (
             <div style={{width: '100%', height: '100%'}}
-                ref={(mount) => {
-                    this.mount = mount
-                }}
+                 ref={(mount) => {
+                     this.mount = mount
+                 }}
             />
         )
     }
